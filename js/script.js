f88fe89e145ec754addf4e88e3655ea5389e6c48
Sumit Das <sumit@example.com>
2025-12-05 14:19:18 +0600

    add

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("#slideUpDown").click(function (){
+       $(".div26").slideToggle(1000);
+    })
+     
+    $("#slideUpDown2").click(function (){
+       $(".div27").slideToggle(1000);
+    })
+    $("#slideUpDown3").click(function (){
+       $(".div27").slideToggle(1000);
+    })
+  
+
+   
+    
+})
+
